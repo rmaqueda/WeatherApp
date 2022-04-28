d979f95a04a67aae03214db7d88157079293ad49
Ricardo Maqueda <rinconestolmo@gmail.com>
2022-04-28 13:30:45 +0200

    Feature/code smells (#8)
    * Add contant plist
    
    * Rename Constant to Constants
    
    * Some compile optimizations
    
    * Fix issue in Swiftlint script
    
    * Add end of line in shell scripts
    
    * Fix simulator to iPhone 13 and update Fastlane
    
    * Add new quality cheks
    
    * Fix project name
    
    * Disable snapthots tests
    
    * Change derived data to build folder
    
    * Remove GitHub token
    
    * Change SonarCloud step
    
    * Revert SonarCloud step to native
    
    * Remove Sonar Key check
    
    * Update sonar sources

--- Scripts/Build-Phases/copy_secrets.sh
+++ Scripts/Build-Phases/copy_secrets.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 set -e
 
 SECRET_FILE=$SRCROOT/$PRODUCT_NAME/Preferences/Secrets.plist
 SAMPLE_SECRET_FILE=$SRCROOT/$PRODUCT_NAME/Preferences/Secrets_example.plist
+
+START_DATE=$(date +"%s")
 
 if [ ! -f "$SECRET_FILE" ]; then
   echo "error: Setup API KEY in Secrets.plist file."
@@ -13,3 +16,7 @@ if echo $OUTPUT | grep "Enter"; then
   echo "error: Setup API KEY in Secrets.plist file."
   exit 1
 fi
+
+END_DATE=$(date +"%s")
+DIFF=$(($END_DATE - $START_DATE))
+echo "Copy secrets took $(($DIFF / 60)) minutes and $(($DIFF % 60)) seconds to complete."
